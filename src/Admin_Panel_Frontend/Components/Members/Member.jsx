1034d765f394e3ad7909ed5235b2aa8ff5f9d753
Sanjay123-sharma <sanjukumar595857@gmail.com>
2025-08-28 17:30:46 +0530

    ok fine

--- src/Admin_Panel_Frontend/Components/Members/Member.jsx
+++ src/Admin_Panel_Frontend/Components/Members/Member.jsx
@@ -151,7 +151,18 @@ export default function Member() {
                 onClick={() => setOpenMenu(!openMenu)}
                 className="w-10 h-10 rounded-full bg-gray-300 flex items-center justify-center text-lg"
               >
-                👤
+                  <svg
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 24 24"
+              fill="currentColor"
+              className="size-6"
+            >
+              <path
+                fillRule="evenodd"
+                d="M7.5 6a4.5 4.5 0 1 1 9 0 4.5 4.5 0 0 1-9 0ZM3.751 20.105a8.25 8.25 0 0 1 16.498 0 .75.75 0 0 1-.437.695A18.683 18.683 0 0 1 12 22.5c-2.786 0-5.433-.608-7.812-1.7a.75.75 0 0 1-.437-.695Z"
+                clipRule="evenodd"
+              />
+            </svg>
               </button>
 
               {openMenu && (
@@ -338,13 +349,13 @@ const MembershipPill = ({ membership }) => {
       {membership === true ? (
         <h6>
           <svg
-            class="text-green-600 h-6 w-6"
+            className="text-green-600 h-6 w-6"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className="size-6"
+            
           >
             <path
               strokeLinecap="round"
@@ -356,13 +367,13 @@ const MembershipPill = ({ membership }) => {
       ) : (
         <h6>
           <svg
-            class=" h-6 w-6 text-blue-600"
+            className=" h-6 w-6 text-red-600"
             xmlns="http://www.w3.org/2000/svg"
             fill="none"
             viewBox="0 0 24 24"
             strokeWidth={1.5}
             stroke="currentColor"
-            className="size-6"
+           
           >
             <path
               strokeLinecap="round"
